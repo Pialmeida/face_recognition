fece6a764979901f0a451efa9f5bd35ee88a8f1a
Pedro Almeida <almeida.pedro.ivo@gmail.com>
2021-03-11 11:31:17 -0300

    changes for testing sensor

--- test.py
+++ test.py
@@ -7,40 +7,51 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BOARD)
+
+PIN_TRIGGER = 7
+PIN_ECHO = 11
+
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
+
+GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
 try:
-	  GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
 
-	  PIN_TRIGGER = 7
-	  PIN_ECHO = 11
+    PIN_TRIGGER = 7
+    PIN_ECHO = 11
 
-	  GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-	  GPIO.setup(PIN_ECHO, GPIO.IN)
+    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+    GPIO.setup(PIN_ECHO, GPIO.IN)
 
-	  GPIO.output(PIN_TRIGGER, GPIO.LOW)
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-	  print "Waiting for sensor to settle"
+    while True:
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-	  time.sleep(2)
+        time.sleep(0.00001)
 
-	  print "Calculating distance"
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-	  GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+        while GPIO.input(PIN_ECHO)==0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO)==1:
+            pulse_end_time = time.time()
 
-	  time.sleep(0.00001)
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
+        print('\n\n')
+        print("Distance:",distance,"cm")
+        print('\n\n')
+        time.sleep(2)
 
-	  GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-	  while GPIO.input(PIN_ECHO)==0:
-			pulse_start_time = time.time()
-	  while GPIO.input(PIN_ECHO)==1:
-			pulse_end_time = time.time()
-
-	  pulse_duration = pulse_end_time - pulse_start_time
-	  distance = round(pulse_duration * 17150, 2)
-	  print "Distance:",distance,"cm"
+except KeyboardInterrupt:
+    pass
 
 finally:
-	  GPIO.cleanup()
+    GPIO.cleanup()
 
 
 
